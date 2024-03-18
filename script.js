b623d7b76904aa6a390fad6c70cab064fb916af1
adityamarkad1611 <adityamarkad1601@gmail.com>
2024-03-18 22:51:30 +0530

    last

--- script.js
+++ script.js
@@ -1,40 +1,55 @@
 
+/*let preloader =document.getElementById(preloader)
 
+function loadweb(){
+    preloader.style.display="none"
+}
+
+*/
 const t1=new SplitType(".intro-txt")
 
 gsap.from("#for-fade" ,{
     opacity:0,
-    duration:5
+    duration:5,
+    delay:1
 })
 
 gsap.from(".nav" ,{
     y:10,
+    delay:1
 })
 
 gsap.from(".char", {
     duration:2.5,
     x:-100,
-    opacity:0
+    opacity:0,
+    delay:1
     
 })
 
 gsap.from("#deadpool" ,{
-    y:50,
-    duration:1,
+    y:100,
+    duration:2,
     
 } )
-
 
 gsap.from(".circle" ,{
     y:100,
     duration:2,
+    delay:1,
     opacity:0
 } )
 
 gsap.from("#name", {
     duration:1,
     y:50,
+    delay:1,
     opacity:0
+})
+
+gsap.from("#preloader" ,{
+    opacity:1,
+    duration:4
 })
 
 
@@ -44,7 +59,7 @@ const t2=new SplitType(".info-txt")
 var t3  = gsap.timeline({
     scrollTrigger: {
         trigger: ".page3-part1",
-        markers: "false",
+        markers: false,
         start: "0% 50%",
         end: "0% 50%",
         scrub: 1,
@@ -62,7 +77,7 @@ t3.from("#info-txt" , {
 var t4  = gsap.timeline({
     scrollTrigger: {
         trigger: ".page3-part2",
-        markers: "false",
+        markers: false,
         start: "0% 50%",
         end: "0% 50%",
         scrub: 1,
