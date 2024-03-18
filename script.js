ecc9931ce76c90ad19320fa3b1355f3f3a98d6e2
adityamarkad1611 <adityamarkad1601@gmail.com>
2024-03-18 19:02:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+const t1=new SplitType(".intro-txt")
+
+gsap.from("#for-fade" ,{
+    opacity:0,
+    duration:5
+})
+
+
+gsap.from(".char", {
+    duration:2.5,
+    x:-100,
+    opacity:0
+    
+})
+
+gsap.from("#deadpool" ,{
+    y:50,
+    duration:1,
+    
+} )
+
+
+gsap.from(".circle" ,{
+    y:100,
+    duration:2,
+    opacity:0
+} )
+
+gsap.from("#name", {
+    duration:1,
+    y:50,
+    opacity:0
+})
+
+
+
+const t2=new SplitType(".info-txt")
+
+var t3  = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page3-part1",
+        markers: "true",
+        start: "0% 50%",
+        end: "0% 50%",
+        scrub: 1,
+        pin: true,
+        duration:5
+    }
+});
+
+t3.from("#info-txt" , {
+    y:200,
+    opacity:0
+    
+})
+
+var t4  = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".page3-part2",
+        markers: "true",
+        start: "0% 50%",
+        end: "0% 50%",
+        scrub: 1,
+        pin: true,
+        duration:5
+    }
+});
+
+t4.from("#fade" ,{
+   opacity:0,
+   x:200
+})
+
+gsap.to(".background-page2" ,{
+    x:"-50%",
+    duration:5,
+    repeat:-1,
+    ease: "none"
+    
+})
+
+gsap.to(".scroll-background" ,{
+    x:"-50%",
+    duration:5,
+    repeat:-1,
+    ease: "none"
+    
+})
+
+
+
+
+
+
+
